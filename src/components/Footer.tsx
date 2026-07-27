39b3364737d6782ad6c53d9a5b2db9da7b2761c4
Edi Septriyanto <me@masedi.net>
2026-07-27 18:49:34 +0700

    Update Landing Page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -36,7 +36,7 @@ const Footer: React.FC = () => {
       </div>
       <div className="mt-8 pt-8 border-t border-gray-700 text-center text-gray-500">
         <p className="mb-4 text-sm max-w-4xl mx-auto px-6">
-          Disclaimer: PT. Inovasi Kota Nabi adalah platform/konsultan travel online yang bermitra dengan penyelenggara perjalanan resmi (PPIU/PIHK), dan bukan instansi pemerintah atau penerbit visa langsung.
+          Disclaimer: PT. Inovasi Kota Nabi adalah platform/konsultan ibadah umroh & haji online yang bermitra dengan travel penyelenggara perjalanan ibadah (PPIU/PIHK).
         </p>
         <p>&copy; {new Date().getFullYear()} {siteConfig.name}. All Rights Reserved.</p>
       </div>
